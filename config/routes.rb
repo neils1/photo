e1c0b352020741e593d7f95f9353bdc34c982986
Administrator <example@mail.com>
2014-03-13 21:55:02 +0800

    complete commontator gem install

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Photo::Application.routes.draw do
   devise_for :users
   root "pins#index"
   get "about" => "pages#about"
+  mount Commontator::Engine => '/commontator'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
